429771d36c93bdb101a90803c44333c867c8fcde
Hayoun Lee <sidhlee@gmail.com>
2020-09-16 22:48:09 +0900

    Add script for menu open / close

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles/main.scss';
+
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+const backdrop = document.querySelector('.backdrop');
+const nav = document.querySelector('nav');
+
+const closeNav = () => {
+  nav.classList.remove('is-open');
+  backdrop.classList.remove('is-open');
+  menuBtnOpen.classList.remove('is-open');
+};
+
+const openNav = () => {
+  nav.classList.add('is-open');
+  backdrop.classList.add('is-open');
+  menuBtnOpen.classList.add('is-open');
+};
+
+menuBtnOpen.addEventListener('click', openNav);
+menuBtnClose.addEventListener('click', closeNav);
+backdrop.addEventListener('click', closeNav);
